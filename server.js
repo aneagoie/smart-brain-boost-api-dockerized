a7efbdd56c9fbfb29700c3398286dedd38e56166
aneagoie <aneagoie1@gmail.com>
2018-04-12 14:54:02 -0400

    FEATURE: dockerizing the whole backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -13,17 +14,24 @@ const auth = require('./controllers/authorization');
 //Database Setup
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
 
-app.use(cors())
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(morgan('combined'));
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
